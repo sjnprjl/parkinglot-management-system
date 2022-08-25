d4658fcbe185df9bc34d91b7b1d26ec68b6208b0
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 15:05:47 +0545

    make default version to 1

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
+    defaultVersion: '1',
   });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
