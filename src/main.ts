a57f5c75f4e04a8618fc931ae3697634923dafc6
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 14:02:22 +0545

    enable versioning

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
