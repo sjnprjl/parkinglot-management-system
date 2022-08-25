a966ff4e24ad3e56360c966a9e475fe9d206907a
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 19:13:40 +0545

    test: location module

--- src/location/location.controller.spec.ts
+++ src/location/location.controller.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationController } from './location.controller';
+import { LocationService } from './location.service';
 
 describe('LocationController', () => {
-  let controller: LocationController;
+  let locationController: LocationController;
 
+  let mockLocationService = {};
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [LocationController],
+      providers: [
+        {
+          provide: LocationService,
+          useValue: mockLocationService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<LocationController>(LocationController);
+    locationController = moduleRef.get<LocationController>(LocationController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+
+  it('should be defined ', () => {
+    expect(locationController).toBeDefined();
   });
 });
