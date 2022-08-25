4d566e6426d954cd7b0ef2f27bd6eb86ad0b12c1
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 13:57:56 +0545

    feat: make user entity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  superadmin = 'godUser',
+  admin = 'admin',
+  normal = 'normal',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true })
+  fullName: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Exclude({ toPlainOnly: true })
+  @Column({ enum: UserRole, default: UserRole.normal })
+  role: UserRole;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
