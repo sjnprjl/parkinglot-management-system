0cf2d0e4a323cdf8fc1a0377522128f623ab7220
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 13:58:23 +0545

    feat; create roles guard

---
+++ src/shared/guards/roles.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRole = this.reflector.getAllAndOverride(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    console.log('requiredRole:', requiredRole);
+    if (!requiredRole) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRole.includes(user?.role);
+  }
+}
