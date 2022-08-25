e0c43330335ad05b94b09956e31dbfcf623eaa95
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 14:29:13 +0545

    fix: add config module for adding options from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: [`${__dirname}/**/*{.ts,.js}`],
-      synchronize: true,
-      database: 'plms_db',
-      password: 'localhost',
-      username: 'postgres',
-      port: 5432,
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        entities: [`${__dirname}/**/*{.ts,.js}`],
+        synchronize: true,
+        database: configService.get('DB_NAME'),
+        password: configService.get('DB_PASSWORD'),
+        username: configService.get('DB_USERNAME'),
+        port: +configService.get('DB_PORT'),
+      }),
     }),
   ],
   controllers: [AppController],
