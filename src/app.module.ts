0896b6608daa5403f2d693e092be2a1717929469
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-25 14:01:13 +0545

    Group all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [`${__dirname}/**/*{.ts,.js}`],
+      synchronize: true,
+      database: 'plms_db',
+      password: 'localhost',
+      username: 'postgres',
+      port: 5432,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
