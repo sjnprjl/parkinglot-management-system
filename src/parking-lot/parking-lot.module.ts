c199770ac2e799f198f27b9c5ff3f50385db967c
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-28 00:30:43 +0545

    export: providers

--- src/parking-lot/parking-lot.module.ts
+++ src/parking-lot/parking-lot.module.ts
@@ -3,10 +3,14 @@ import { ParkingLotService } from './parking-lot.service';
 import { ParkingLotController } from './parking-lot.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ParkingLot } from './entities/parking-lot.entity';
+import { IsAdminGuard } from './guards/is-admin.guard';
+import { ParkingLotParkingSpotType } from './entities/parking-lot_parking-type.entity';
+import { ParkingTypeModule } from 'src/parking-type/parking-type.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ParkingLot])],
-  providers: [ParkingLotService],
+  imports: [ParkingTypeModule, TypeOrmModule.forFeature([ParkingLot, ParkingLotParkingSpotType])],
+  providers: [ParkingLotService, IsAdminGuard],
   controllers: [ParkingLotController],
+  exports: [IsAdminGuard, ParkingLotService],
 })
 export class ParkingLotModule {}
