c683ecf97905cab9f8eb2b1071c95e3e7c503b84
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-28 00:30:15 +0545

    feat: make parking spot charge different for different parking lots

--- src/parking-lot/parking-lot.controller.ts
+++ src/parking-lot/parking-lot.controller.ts
@@ -1,5 +1,6 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -9,20 +10,29 @@ import {
   Post,
   Query,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
-import { Roles } from 'src/shared/decorators/roles.decorator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ParkingTypeService } from 'src/parking-type/parking-type.service';
 import { JwtAuthGuard } from 'src/shared/guards/jwt.guard';
-import { RolesGuard } from 'src/shared/guards/roles.guard';
-import { UserRole } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateParkingLotDto } from './dto/create-parking-lot.dto';
+import { CreateParkingSpotTypeDto } from './dto/create-parking-type.dto';
 import { UpdateParkingLotDto } from './dto/update-parking-lot.dto';
-import { IsAdmin } from './guards/is-admin.guard';
+import { UpdateParkingTypeDto } from './dto/update-parking-type.dto';
+import { ParkingLotParkingSpotType } from './entities/parking-lot_parking-type.entity';
+import { IsAdminGuard } from './guards/is-admin.guard';
 import { ParkingLotService } from './parking-lot.service';
 
 @UseGuards(JwtAuthGuard)
-@Controller('parking-lot')
+@Controller('parking-lots')
 export class ParkingLotController {
-  constructor(private readonly parkingLotService: ParkingLotService) {}
+  constructor(
+    private readonly parkingLotService: ParkingLotService,
+    private readonly parkingTypeService: ParkingTypeService,
+    @InjectRepository(ParkingLotParkingSpotType)
+    private readonly parkingLotParkingSpotTypeRepo: Repository<ParkingLotParkingSpotType>,
+  ) {}
 
   @Get()
   find(@Query() { limit, offset }: { limit: number; offset: number }) {
@@ -34,13 +44,13 @@ export class ParkingLotController {
     return this.parkingLotService.findOne(id);
   }
 
-  @UseGuards(IsAdmin)
+  @UseGuards(IsAdminGuard)
   @Post()
   create(@Body() createParkingLotDto: CreateParkingLotDto) {
     return this.parkingLotService.create(createParkingLotDto);
   }
 
-  @UseGuards(IsAdmin)
+  @UseGuards(IsAdminGuard)
   @Patch('/:id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -49,9 +59,62 @@ export class ParkingLotController {
     return this.parkingLotService.update(id, updateParkingLotDto);
   }
 
-  @UseGuards(IsAdmin)
+  @UseGuards(IsAdminGuard)
   @Delete('/:id')
   delete(@Param('id', ParseUUIDPipe) id: string) {
     return this.parkingLotService.delete(id);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id/parking-types')
+  async getAllTypes(@Param('id') id: string) {
+    return await this.parkingLotParkingSpotTypeRepo.find({
+      where: {
+        parkingLotId: id,
+      },
+      relations: ['parkingSpotType', 'parkingLot'],
+      select: {
+        parkingSpotType: {
+          id: true,
+          type: true,
+        },
+        parkingLot: {
+          id: true,
+          name: true,
+        },
+      },
+    });
+  }
+
+  @UseGuards(IsAdminGuard)
+  @Post('/:id/parking-types')
+  async createParkingSpotType(
+    @Param('id') id: string,
+    @Body() createParkingSpotType: CreateParkingSpotTypeDto,
+  ) {
+    await this.parkingLotService.findOne(id);
+    await this.parkingTypeService.findOne(
+      createParkingSpotType.parkingSpotTypeId,
+    );
+    return await this.parkingLotParkingSpotTypeRepo.save({
+      ...createParkingSpotType,
+      parkingLotId: id,
+    });
+  }
+
+  @UseGuards(IsAdminGuard)
+  @Patch('/:parkingLotId/parking-types/:parkingTypeId')
+  async updateParkingSpotType(
+    @Param('parkingLotId') parkingLotId: string,
+    @Param('parkingTypeId') parkingTypeId: string,
+    @Body() input: UpdateParkingTypeDto,
+  ) {
+    return await this.parkingLotParkingSpotTypeRepo.update(
+      {
+        // parkingLotId,
+        id: parkingTypeId,
+      },
+      input,
+    );
   }
 }
