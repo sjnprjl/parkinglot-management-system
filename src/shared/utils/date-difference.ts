11af37483bae0c675857613d5f06f9facf8e55a9
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-28 22:30:27 +0545

    add: date difference in hours utils func

--- src/shared/utils/date-difference.ts
+++ src/shared/utils/date-difference.ts
@@ -1,11 +1,24 @@
 const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-function dateDiffInDays(dateA: Date, dateB: Date) {
-  // Discard the time and time-zone information.
-  const utc1 = Date.UTC(dateA.getFullYear(), dateA.getMonth(), dateA.getDate());
-  const utc2 = Date.UTC(dateB.getFullYear(), dateB.getMonth(), dateB.getDate());
+export function dateDiffInDays(startDate: Date, endDate: Date) {
+  const utc1 = Date.UTC(
+    startDate.getFullYear(),
+    startDate.getMonth(),
+    startDate.getDate(),
+  );
+  const utc2 = Date.UTC(
+    endDate.getFullYear(),
+    endDate.getMonth(),
+    endDate.getDate(),
+  );
 
   return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
 
-export default dateDiffInDays;
+export function dateDiffInHours(startDate: Date, endDate: Date) {
+  const msInHour = 1000 * 60 * 60;
+
+  return Math.round(
+    Math.abs(endDate.getTime() - startDate.getTime()) / msInHour,
+  );
+}
