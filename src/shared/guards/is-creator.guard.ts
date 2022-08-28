ad3f4f795e79dc1e5f1e371823654b50c86833d9
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-28 09:02:34 +0545

    add: error message when request does not contain user property

--- src/shared/guards/is-creator.guard.ts
+++ src/shared/guards/is-creator.guard.ts
@@ -2,15 +2,16 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { UserRole } from 'src/user/entities/user.entity';
 
-export abstract class AbstractIsCreator implements CanActivate {
+export abstract class AbstractIsAuthorized implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    if (!request?.user) return false;
+    if (!request?.user)
+      throw Error('There is a possibility that you have not used JwtAuthGuard');
     if (request.user.role === UserRole.admin) return true;
-    return this.validate(request);
+    return this.validate(request, context);
   }
 
-  abstract validate(request: any): Promise<boolean>;
+  abstract validate(request: any, context: ExecutionContext): Promise<boolean>;
 }
